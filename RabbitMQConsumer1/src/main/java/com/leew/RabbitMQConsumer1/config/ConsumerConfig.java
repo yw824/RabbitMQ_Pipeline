89e5e21bfe1e9b21d28488f53de71230a2ed2ad1
yw824 <yongwoo25@gmail.com>
2024-10-11 11:31:37 +0900

    feat: configuration 설정
    - application.yml :
    	- RabbitMQ와 연결할 서버 주소, 포트 및 계정 설정
    	- 특정 Broker의 토픽을 생성하기 위한 Exchange, Queue, routing
    - Config.java :
    	- application.yml 파일의 설정값을 Value로 가져옴
    	- 해당 값으로 RabbitMQ 컨테이너와 연결하는 과정을 Bean으로
    	  설정하여
    	  	- SpringBoot 서버가 시작하였을 때 자동으로 호출하여 연결
    		  설정
    	- Jackson2Converter로 DTO나 JSON을 직렬화/역직렬화 구현
    	- RabbitMQ와 연결 및 통신하기 위한 ConnectionFactory +
    	  RabbitTemplate 설정

--- RabbitMQConsumer1/src/main/java/com/leew/RabbitMQConsumer1/config/ConsumerConfig.java
+++ RabbitMQConsumer1/src/main/java/com/leew/RabbitMQConsumer1/config/ConsumerConfig.java
@@ -1,4 +1,4 @@
-package com.leew.RabbitMQProducer.config;
+package com.leew.RabbitMQConsumer1.config;
 
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ProducerConfig {
+public class ConsumerConfig {
 
     // value under is in application.yml
     @Value("${rabbitmq.queue.name}")
